bab578f2275e2139417f6bcf09cf3f95e893a637
James Cook <jamesjcook@users.noreply.github.com>
2019-11-25 13:22:03 -0500

    Fixed bug with setting views before they're initalized
    Now put's data into viewers

--- FiberBundle_Setup.py
+++ FiberBundle_Setup.py
@@ -1,5 +1,10 @@
-#Code for setting up the Fiber Bundles in Two Strain View layout
-#Strain a will be seen on the left side and strain b will be seen on the right side
+#Code for setting up the Fiber Bundles in Fiber Compare Views
+#Input intList is a list of the fiber bundle numbers to display from left to right
+#e.g. list [1,2] displays FiberBundle1 in View 1 (left side) and FiberBundle2 in View 2 (right side)
+#
+#Note that it is necessary to exec a FiberCompareView first before executing code
+#e.g. exec FiveStrainView.py first before giving input list of length five
+#Note that the code assumes that MRML IDs for the FA and Fiber Bundle Nodes share the same number
 #Author: Austin Kao
 def SetUpFiberBundle(intList,trkFilter=None):
 	if(not(type(intList) is list)):
@@ -12,14 +17,19 @@ def SetUpFiberBundle(intList,trkFilter=None):
 	viewNum = 1 #Variable representing the view number
 	sliceString = "" #Variable representing the slice node ID string
 	scene = slicer.app.mrmlScene()
+	volumes = scene.GetNodesByClass("vtkMRMLScalarVolumeNode")
+	#TODO: Ensure volume count is same as intList length
+	layoutManager.setLayout(TwoStrainView)
 	for i in range(len(intList)):
+		#fib = scene.GetNodeByID("vtkMRMLFiberBundleNode"+str(intList[i]))
+		#fib.SetColorModeToMeanFiberOrientation()
 		lineString = "vtkMRMLFiberBundleLineDisplayNode"+str(intList[i])
 		tubeString = "vtkMRMLFiberBundleTubeDisplayNode"+str(intList[i])
 		viewString = "vtkMRMLViewNode"+str(viewNum)
 		if(viewNum==1):
 			sliceString = "vtkMRMLSliceNodeOne"
 		elif(viewNum==2):
-			layoutManager.setLayout(TwoStrainView)
+			
 			sliceString = "vtkMRMLSliceNodeTwo"
 		elif(viewNum==3):
 			layoutManager.setLayout(ThreeStrainView)
@@ -30,6 +40,8 @@ def SetUpFiberBundle(intList,trkFilter=None):
 		elif(viewNum==5):
 			layoutManager.setLayout(FiveStrainView)
 			sliceString = "vtkMRMLSliceNodeFive" 
+		compString = sliceString.replace("Slice","SliceComposite")
+		print(compString)
 		line1 = scene.GetNodeByID(lineString)
 		line1.SetDisplayableOnlyInView(viewString)
 		tube1 = scene.GetNodeByID(tubeString)
@@ -37,4 +49,9 @@ def SetUpFiberBundle(intList,trkFilter=None):
 		tube1.SetTubeRadius(0.05)
 		line1.VisibilityOn()
 		tube1.VisibilityOn() #Note that the tube slice will not show up
+		line1.SetColorModeToPointFiberOrientation()
+		tube1.SetColorModeToPointFiberOrientation()
+		compNode = scene.GetNodeByID(compString)
+		volume = volumes.GetItemAsObject(intList[i]-1)
+		compNode.SetBackgroundVolumeID(volume.GetID())
 		viewNum = viewNum+1
